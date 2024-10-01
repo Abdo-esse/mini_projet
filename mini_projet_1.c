64fc36663d3ab8a0634eae9e422f589effdcbe10
Abdo-esse <esseabdo37@gmail.com>
2024-10-01 02:11:38 +0100

    J'ai trasfere les fonctionnalites de ce programme sous forme d'une menu

--- mini_projet_1.c
+++ mini_projet_1.c
@@ -1,66 +1,76 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include<string.h>
 
 
-int main()
-{
 
+int main(){
 
-    char titre[100];
-    char auteur[100];
-    float prix [100];
+    char titre[100][100];
+    char auteur [100][100];
+    float prix [1000];
     float quantite[100];
-    char titre2[100];
-    int i,nombre , book_count;
+    int nbr=0;
 
 
-    printf("Veuillez entrer le nombrer des livres que vous souhaitez stockees: ");
-    scanf("%d",&nombre);
-
-    printf("Entrer les titres des livres (sans espace):\n");
-
-    for(i=0;i<nombre;i++){
-        printf("- Titre du livre [%d] : ",i+1);
-        scanf("%s",&titre[i]);
-         printf("- Auteur du livre [%d] : ",i+1);
-        scanf("%s",&auteur[i]);
-        printf("- Prix du livre [%d] : ",i+1);
-        scanf("%f",&prix[i]);
-        printf("- Quantite en stock [%d] : ",i+1);
-        scanf("%f",&quantite[i]);
-    }
-    printf("les Livres Disponibles : \N");
-    printf("============================================\n");
-
-    for(i=0;i<nombre;i++){
-
-        printf("- Titre du livre [%d] :%s \n",i+1,titre);
-
-        printf("- Auteur du livre [%d] :%s \n",i+1,auteur);
-
-        printf("- Prix du livre [%d] :%d \n",i+1,prix);
-
-        printf("- Quantite en stock [%d] :%d \n",i+1,quantite);
-
-        printf("============================================\n");
-    }
-    printf("veuillez entrer lr titre dutableae : ");
-    scanf("%s",&titre2);
+    int c;
 
 
-        if (strstr(titre,titre2) !='\0'){
-            printf("existre");
+    do{
+        printf("\t Menu\n");
+        printf("===============================\n");
+        printf("1-Ajouter un livre.\n");
+        printf("2-Aficher tous livres disponibles.\n ");
+        printf("3-Modifier la quantite d'un livre.\n");
+        printf("4-Quitter.\n");
+        printf("Entrer votre choix : ");
+        scanf("%d",&c);
+        getchar();
+
+        if(c == 1){
+            if (nbr<100){
+                printf("Titre du livre:");
+                fgets(titre[nbr], 100, stdin);
+                titre[nbr][strcspn(titre[nbr], "\n")] = '\0';
+                printf("Auteur du livre:");
+                fgets(auteur[nbr],100,stdin);
+                auteur[nbr][strcspn(auteur[nbr], "\n")] ='\0';
+                printf("Prix du livre: ");
+                scanf("%f",&prix[nbr]);
+                printf("Quantite du livre: ");
+                scanf("%f",&quantite[nbr]);
+                 getchar();
+                 nbr++;
+                }
+            else
+                ("le stocke est plien");
+        }
+        else if(c == 2){
+                if (nbr == 0){
+                    printf("Aucun livre disponible dans le stock.\n");
+                }
+                 else{
+                    for(int i=0;i<nbr;i++){
+                       printf("Livre %d\n",i+1);
+                       printf("Titre: %s\n",titre[i]);
+                       printf("Auteur:%s\n",auteur[i]);
+                       printf("Prix:%.2f \n",prix[i]);
+                       printf("Quantite:%.f \n",quantite[i]);
+              }
+        }
         }
 
+        else if(c == 3){
+                printf("Pas encore ajoute\n");
 
+        }
+         else if(c == 4){
+                printf("Good by");
+         }
 
-
-
-
-
-
-
+        else
+            ("Erreur,veuillez réessayer\n");
+    } while (c != 4);
 
     return 0;
 }
-
